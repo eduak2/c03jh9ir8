f7f20749b6f8d03f1ae552d2023099eca1e2c82f
eduak2 <evizcaino295@gmail.com>
2017-09-07 15:33:31 -0500

    link_to with actors_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get 'actors/'  => 'actors#index'
-  get  'actors/new', to: 'actors#new', as: :new_actors
+  get  'actors/new' => 'actors#new'
   post 'actors/' =>  'actors#create'
   
   root 'actors#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
