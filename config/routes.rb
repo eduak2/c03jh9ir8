464d15c3d350ce010ada223e70c30f643bb3c138
eduak2 <evizcaino295@gmail.com>
2017-09-07 15:38:58 -0500

    Change name root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'actors/'  => 'actors#index'
-  get  'actors/new' => 'actors#new'
+  get  'actors/new', to: 'actors#new', as: :new_actors_path 
   post 'actors/' =>  'actors#create'
   
   root 'actors#index'
